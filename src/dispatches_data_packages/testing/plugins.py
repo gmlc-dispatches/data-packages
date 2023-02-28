b0d4f528e9a25c21aa27a76bad0043de5023a74a
Ludovico Bianchi <lbianchi@lbl.gov>
2023-02-28 14:17:29 -0600

    Add test item for required text

--- src/dispatches_data_packages/testing/plugins.py
+++ src/dispatches_data_packages/testing/plugins.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+from typing import Dict
+from typing import Iterable
 from typing import List
 
 import pytest
@@ -13,10 +15,9 @@ def _is_nonempty_file(p: Path, min_size_bytes: int = 1) -> bool:
 
 
 class DataPackage(pytest.Item):
-    def __init__(self, *, key, required: List[Path], **kwargs):
+    def __init__(self, *, key: str, **kwargs):
         super().__init__(**kwargs)
         self.key = key
-        self.required = list(required)
 
     def runtest(self):
         available = api.available()
@@ -25,21 +26,36 @@ class DataPackage(pytest.Item):
         path = api.path(self.key)
         if not path:
             raise LookupError("Could not find path")
-        missing = []
-        for fname in self.required:
-            fpath = path / fname
-            if not _is_nonempty_file(fpath):
-                missing.append(fpath)
-        if missing:
-            raise ValueError(f"The following files are required but are missing: {missing}")
 
     def reportinfo(self):
         return self.name, 0, self.key
 
 
+class RequiredContent(pytest.Item):
+    def __init__(self, *, key: str, file_name: str, text: str, **kwargs):
+        super().__init__(**kwargs)
+        self.key = key
+        self.file_name = file_name
+        self.text = text
+
+    def runtest(self):
+        fpath = api.path(self.key, resource=self.file_name)
+        
+        if not fpath.is_file():
+            raise FileNotFoundError(f"No file named {self.file_name!r} found for {self.key}")
+        file_text = fpath.read_text()
+        if not file_text:
+            raise ValueError(f"No text found in {fpath}")
+        if not self.text.lower() in file_text.lower():
+            raise ValueError(f"Required text {self.text!r} not found in {self.file_name}")
+
+    def reportinfo(self):
+        return self.name, 0, self.text
+
+
 class DataPackagePlugin:
-    def __init__(self, required_files):
-        self.required_files = list(required_files)
+    def __init__(self, required: Dict[str, str]):
+        self.required = required
         self._to_check = []
 
     def pytest_addoption(self, parser):
@@ -48,21 +64,26 @@ class DataPackagePlugin:
     def pytest_configure(self, config):
         self._to_check = list(config.option.data_packages)
 
+    def _create_test_items(self, key: str, parent) -> Iterable[pytest.Item]:
+        for_package = DataPackage.from_parent(
+            parent,
+            name=key,
+            key=key,
+        )
+        yield for_package
+        for text, fname in self.required.items():
+            yield RequiredContent.from_parent(
+                for_package,
+                name=f"{fname}:{text}",
+                key=key,
+                file_name=fname,
+                text=text,
+            )
+
     def pytest_collection_modifyitems(self, session, items):
         for key in self._to_check:
-            item = DataPackage.from_parent(
-                session,
-                name=f"data_packages::{key}",
-                key=key,
-                required=list(self.required_files),
-            )
-            items.append(item)
+            for item in self._create_test_items(key, session):
+                items.append(item)
 
 
-plugin = DataPackagePlugin(
-    required_files=[
-        "LICENSE.md",
-        "__init__.py",
-        "README.md",
-    ],
-)
+plugin = DataPackagePlugin(required={"copyright": "README.md", "license": "README.md"})
